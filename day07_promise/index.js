565f82d42d104fac65585a70dec7fb1fa39c823d
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2023-01-22 23:54:17 +0530

    promise done

---
+++ day07_promise/index.js
@@ -0,0 +1,19 @@
+
+// Promise
+
+// They will be fullfilled
+// They will be not fullfilled
+// They will be in process
+
+var Promise = new Promise();
+
+// Resolve()
+
+// Reject()
+
+
+var mypromise = new Promise(function(resolve,reject) {
+    resolve('success');
+    reject('error');
+})
+
