9a82cd50f59f0a2ab52e0fd8c2608031e9b5d601
shubhamkmit9021 <shubhamkmit9021@gmail.com>
2023-01-30 23:45:45 +0530

    synchronous and asynchronous example

---
+++ day15_synchronous/index.js
@@ -0,0 +1,15 @@
+
+// Synchronous
+
+// console.log("first");
+// console.log("second");
+// console.log("third");
+
+// Asynchronous
+
+console.log("first");
+setTimeout( () => {
+    console.log("second");
+},5000) 
+
+console.log("third");
